7e02dfe5c157af7fb33b45954e523f0bc3174905
Donny <bajracharyanimesh2022@gmail.com>
2024-08-26 11:11:11 +0545

    All files included

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+function Home() {
+    return(
+        <div>
+            <h2>Dashboard</h2>
+                <nav>
+                    <Link to = 'profile'>Profile</Link>
+                    <Link to = 'settings'>Settings</Link>
+                </nav>
+                <Outlet />
+        </div>
+    )
+}
+export default Home;
---
+++ src/Settings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Settings = () => {
+  return (
+    <div>
+        <h2>This is the Settings Page.</h2>
+        <p>And yes it is working just fine!</p>
+    </div>
+  )
+}
+export default Settings;
